6960dac26dd7b5b5f06c9473460e7e080fd436b6
Soner Yeşilay <soneryesilay@outlook.com>
2023-03-22 14:48:53 +0300

    Add project files.

---
+++ LogicalAndRelationalOperators/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        txtnot1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(165, 117)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(140, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Ders ortalamanız = "' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(311, 202)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(103, 72)
+        Button1.TabIndex = 1
+        Button1.Text = "Kaydet"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txtnot1
+        ' 
+        txtnot1.Location = New Point(311, 117)
+        txtnot1.Name = "txtnot1"
+        txtnot1.Size = New Size(103, 27)
+        txtnot1.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtnot1)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtnot1 As TextBox
+End Class
---
+++ LogicalAndRelationalOperators/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim iNot As Integer
+        iNot = txtnot1.Text
+
+        If iNot < 0 Or iNot > 100 Then
+            MsgBox("Lütfen 0 ile 100 arasında bir not girin!")
+            Exit Sub 'PROGRAMI DURDURDU'
+        End If
+
+
+
+        If iNot <= 100 And iNot >= 90 Then
+            MsgBox("HARF NOTUNUZ AA")
+
+        ElseIf iNot < 90 And iNot >= 80 Then
+            MsgBox("HARF NOTUNUZ AB")
+
+        ElseIf iNot < 80 And iNot >= 70 Then
+            MsgBox("HARF NOTUNUZ BB")
+
+        ElseIf iNot < 70 And iNot >= 60 Then
+            MsgBox("HARF NOTUNUZ BC")
+
+        ElseIf iNot < 60 And iNot >= 50 Then
+            MsgBox("HARF NOTUNUZ CC")
+
+        ElseIf iNot < 50 And iNot >= 40 Then
+            MsgBox("HARF NOTUNUZ CD")
+
+        ElseIf iNot < 40 And iNot >= 30 Then
+            MsgBox("HARF NOTUNUZ DD")
+
+        ElseIf iNot < 30 And iNot >= 20 Then
+            MsgBox("HARF NOTUNUZ DF")
+
+        ElseIf iNot < 20 And iNot >= 0 Then
+            MsgBox("HARF NOTUNUZ FF")
+
+        End If
+
+        If iNot >= 50 Then
+            MsgBox("DERSİ GEÇTİNİZ")
+
+        Else
+            MsgBox("DERSTEN KALDINIZ")
+            Exit Sub
+        End If
+
+
+    End Sub
+End Class
